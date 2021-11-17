26862e62454416698c51cc5a899e918da40322a7
Aaron Lin <aaronlin74@gmail.com>
2021-11-18 10:02:29 +1100

    add mongoose and modify the basic setup

--- routes/user.js
+++ routes/user.js
@@ -2,4 +2,25 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 
+router.get('/', async (req, res) => {
+  try {
+    const user = await User.find()
+
+    res.json(user)
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.post('/', async (req, res) => {
+  const user = new User({
+    username: req.body.username,
+  })
+  try {
+    const newUser = await user.save()
+    res.status(201).json(newUser)
+  } catch (err) {
+    console.log(err)
+  }
+})
 module.exports = router
--- server.js
+++ server.js
@@ -2,17 +2,27 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose')
+const userRouter = require('./routes/user')
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.json())
+app.use('/api/users', userRouter)
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
-});
-
-
-
-
-
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
 })
+
+const start = async () => {
+  try {
+    mongoose.connect(process.env.DATABASE_URL)
+    const listener = app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is listening on port ${listener.address().port}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
